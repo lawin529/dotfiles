2b55495dd12af0a3987d50fd4653dbea3581258e
w <winsonl4272@gmail.com>
2023-05-08 14:06:40 -0700

    adding orgmode config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,12 +6,20 @@ require('orgmode').setup_ts_grammar()
 require('nvim-treesitter.configs').setup {
   highlight = {
     enable = true,
-    additional_vim_regex_highliting = {'org'},
+    additional_vim_regex_highliting = { 'org' },
   },
-  ensure_installed = {'org'},
+  ensure_installed = { 'org' },
 }
 
 require('orgmode').setup({
   org_agenda_files = '~/school/org/*',
   org_default_notes_file = '~/school/org/refile.org',
+  org_deadline_warning_days = 21,
+  org_hide_leading_stars = true,
+  org_todo_keywords = { 'TODO', 'QUIZ', 'EXAM', '|', 'DONE', 'WRITTEN' },
+  org_agenda_start_on_weekday = false,
+  org_agenda_start_day = "-2d",
+  org_agenda_skip_scheduled_if_done = true,
+  org_agenda_skip_deadline_if_done = true,
+  org_indent_mode = noindent
 })
