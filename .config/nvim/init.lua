1119e5b4f1537e5bcab6290c48c58024a116d61e
w <winsonl4272@gmail.com>
2023-05-23 22:57:07 -0500

    updating orgmode config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -17,6 +17,13 @@ require('orgmode').setup({
   org_deadline_warning_days = 21,
   org_hide_leading_stars = true,
   org_todo_keywords = { 'TODO', 'QUIZ', 'EXAM', '|', 'DONE', 'WRITTEN' },
+  org_todo_keyword_faces = {
+    TODO = ':foreground lightred',
+    QUIZ = ':foreground blue :weight bold :underline on',
+    EXAM = ':foreground darkred :weight bold :underline on',
+    DONE = ':foreground green',
+    WRITTEN = ':foreground green',
+  },
   org_agenda_start_on_weekday = false,
   org_agenda_start_day = "-2d",
   org_agenda_skip_scheduled_if_done = true,
